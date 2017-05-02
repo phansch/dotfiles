a734a413315d63b62f95f9b3cd8a0b082bf5c342
Philipp Hansch <dev@phansch.net>
2017-05-02 16:29:49 +0200

    Fix work layout

--- screenlayout/work_default.sh
+++ screenlayout/work_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x368 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode None --pos 1920x0 --rotate left
+xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x352 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1920x1200 --pos 1920x0 --rotate left
