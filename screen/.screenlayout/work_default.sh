509e90cc36230c4250209efd970661cd708d82ff
Philipp Hansch <dev@phansch.net>
2017-11-29 11:29:51 +0100

    Update work screen layout
    Moved screens around

--- screen/.screenlayout/work_default.sh
+++ screen/.screenlayout/work_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1-1 --mode 1920x1080 --primary --output DP-1-3 --rotate left --mode 1920x1200 --right-of DP-1-1 --output eDP-1 --mode 1920x1080 --left-of DP-1-1 --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-2 --off
+xrandr --output DP-1-1 --mode 1920x1080 --primary --output DP-1-3 --rotate left --mode 1920x1200 --left-of DP-1-1 --output eDP-1 --mode 1920x1080 --right-of DP-1-1 --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-2 --off
