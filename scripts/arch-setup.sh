5c03e09b546722732a9f39eedbe51d9152b94f19
Philipp Hansch <dev@phansch.net>
2018-01-31 15:39:37 +0100

    Remove rvm from arch setup

--- scripts/arch-setup.sh
+++ scripts/arch-setup.sh
@@ -14,9 +14,6 @@ sudo pacman -S htop
 sudo pacman -S skype
 sudo pacman -S mupdf
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 mkdir ~/builds
 
 cd ~/builds || return
