377f035fb70172d904f446d5e039644ee2a1fc32
Philipp Hansch <dev@phansch.net>
2018-02-03 00:01:11 +0100

    Make screenlayout scripts more reliable

--- screen/.screenlayout/home_default.sh
+++ screen/.screenlayout/home_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --off
+xrandr && xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --off
