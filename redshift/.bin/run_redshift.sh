60d00c6f055059835b3bbead15596d11e05cb05d
Philipp Hansch <dev@phansch.net>
2018-02-14 08:18:01 +0100

    Attempt to fix redshift script startup
    Closes #55

--- redshift/.bin/run_redshift.sh
+++ redshift/.bin/run_redshift.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! pgrep -f redshift > /dev/null
+if ! pgrep -x redshift > /dev/null
 then
-  redshift -l 52.2:13.6 -t 6500:3400 -m randr -v
+  redshift -l 52.2:13.6 -t 6500:3400 -m randr -v &
 fi
