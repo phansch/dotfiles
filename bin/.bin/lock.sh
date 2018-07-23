ad79fb7366d46d2cfe53a83a22cdd515f9ab847f
Philipp Hansch <dev@phansch.net>
2018-07-23 08:17:39 +0200

    [travis] Fix shellcheck issues

--- bin/.bin/lock.sh
+++ bin/.bin/lock.sh
@@ -9,7 +9,7 @@ scrot "$cfg_tmpdir/screen.png"
 
 convert "$image_path" -blur 4x8 "$image_path"
 
-j "[`hostname`] Locking Screen"
+j "[$(hostname)] Locking Screen"
 i3lock -u -n -t -i "$image_path"
 
-j "[`hostname`] Unlocking screen"
+j "[$(hostname)] Unlocking screen"
