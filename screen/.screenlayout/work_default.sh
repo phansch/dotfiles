3f056a2263c769e1f26d24602e4fdab9ced2f3f5
Philipp Hansch <dev@phansch.net>
2019-04-23 10:58:26 +0200

    Update work screens orientation
    Moved them around again =)

--- screen/.screenlayout/work_default.sh
+++ screen/.screenlayout/work_default.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xrandr && xrandr --output DP-1-1 --mode 1920x1080 --primary \
-  --output DP-1-3 --rotate left --mode 1920x1200 --right-of DP-1-1 \
-  --output eDP-1 --mode 1920x1080 --left-of DP-1-1 \
+  --output DP-1-3 --rotate left --mode 1920x1200 --left-of DP-1-1 \
+  --output eDP-1 --mode 1920x1080 --right-of DP-1-1 \
   --output DP-1 --off \
   --output HDMI-1 --off \
   --output DP-2 --off \
