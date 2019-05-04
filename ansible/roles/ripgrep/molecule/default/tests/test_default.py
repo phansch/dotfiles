68648c1293701cd9018c47b6073e931cfe5e3df7
Philipp Hansch <dev@phansch.net>
2019-05-04 09:42:02 +0200

    Fix idempotency check
    Queries are fine to be marked as `changed_when: false` because they will
    always return some information and it doesn't make sense to not return
    the information.

--- ansible/roles/ripgrep/molecule/default/tests/test_default.py
+++ ansible/roles/ripgrep/molecule/default/tests/test_default.py
@@ -6,9 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_ripgrep_present(host):
+    ripgrep = host.package('ripgrep')
+    assert ripgrep.is_installed
