b57d35b088fd8003194aac42389e0fa81a728d19
Philipp Hansch <dev@phansch.net>
2019-05-09 07:25:35 +0200

    Extract markdown_mimetype role

---
+++ ansible/roles/markdown_mimetype/molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/root/.local/share/mime/packages/text-markdown.xml')
+
+    expected_string = """<?xml version="1.0"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="text/plain">
+    <glob pattern="*.md"/>
+    <glob pattern="*.mkd"/>
+    <glob pattern="*.markdown"/>
+  </mime-type>
+</mime-info>"""
+    assert f.exists
+    assert f.content_string == expected_string
+    assert f.user == 'root'
+    assert f.group == 'root'
