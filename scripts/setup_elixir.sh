da142a9a84d1066ad5e879320d8fc1d8318c92f4
Philipp Hansch <dev@phansch.net>
2019-12-12 18:50:20 +0100

    Remove setup_elixir.sh script
    Haven't been doing Elixir in 3 years.

--- scripts/setup_elixir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir
-asdf install elixir 1.2.4
-asdf global elixir 1.2.4
-asdf install erlang 18.3
-asdf global erlang 18.3
