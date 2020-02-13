5c1c9ae80901cb305552b393c44c8937a5afaf60
Philipp Hansch <dev@phansch.net>
2020-02-13 10:07:55 +0100

    New screenlayout at work

--- screen/.screenlayout/work_default.sh
+++ screen/.screenlayout/work_default.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xrandr && xrandr --output DP-1-1 --mode 1920x1080 --scale 1x1 --primary \
-  --output DP-1-3 --rotate left --mode 1920x1200 --scale 1x1 --left-of DP-1-1 \
-  --output eDP-1 --scale 0.8x0.8 --mode 1920x1080 --right-of DP-1-1 \
+  --output DP-1-3 --rotate left --mode 1920x1200 --scale 1x1 --right-of DP-1-1 \
+  --output eDP-1 --scale 0.8x0.8 --mode 1920x1080 --right-of DP-1-3 \
   --output DP-1 --off \
   --output HDMI-1 --off \
   --output DP-2 --off \
