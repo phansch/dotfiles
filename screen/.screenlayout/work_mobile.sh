8370a4bf16a6d4b4e9d053d6501a2ce7c2d3ab4a
Philipp Hansch <dev@phansch.net>
2020-03-27 13:41:56 +0100

    [screen] Update work_mobile screenlayout

--- screen/.screenlayout/work_mobile.sh
+++ screen/.screenlayout/work_mobile.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-xrandr && xrandr --output DP-1-1 --off --output DP-1-3 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-2 --off --output eDP-1 --mode 1920x1080
+xrandr && xrandr \
+  --output DP-1-1 --off \
+  --output DP-1-3 --off \
+  --output DP-1 --off \
+  --output HDMI-1 --off \
+  --output DP-2 --off \
+  --output HDMI-2 --off \
+  --output DP-1-2 --off \
+  --output DVI-I-1-1 --off \
+  --output eDP-1 --mode 1920x1080 \
+  --verbose
