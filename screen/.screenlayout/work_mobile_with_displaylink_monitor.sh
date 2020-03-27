b7f2ac6bd37d78d87fd2a596f19c0e21c9f3e4cf
Philipp Hansch <dev@phansch.net>
2020-03-27 13:44:13 +0100

    [screen] Add screenlayout for displaylink monitor

---
+++ screen/.screenlayout/work_mobile_with_displaylink_monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xrandr && xrandr \
+  --output DP-1-1 --off \
+  --output DP-1-3 --off \
+  --output DP-1 --off \
+  --output HDMI-1 --off \
+  --output DP-2 --off \
+  --output HDMI-2 --off \
+  --output DP-1-2 --off \
+  --output eDP-1 --primary --mode 1920x1080 --scale 1.0x1.0 \
+  --output DVI-I-1-1 --mode 1920x1080 --right-of eDP-1 --reflect normal --rotate normal --scale 1.0x1.0 \
+  --verbose
