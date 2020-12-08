f8a1c610b6ec9763fb29b18a2d7d763007720343
Philipp Hansch <dev@phansch.net>
2020-12-08 08:58:45 +0100

    Add separate work-from-home screenlayout
    Because the two laptops call the monitor output differently AND I have
    to first turn of the monitor before it can be connected again.

---
+++ screen/.screenlayout/work_from_home.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Need to turn the output off an on again to make it work.
+# Without this, it would not turn on.
+xrandr --output DP-1-1 --off
+xrandr && xrandr \
+  --output DP-1-1 --mode 1920x1080 --pos 0x0 --primary --rotate normal \
+  --output eDP-1 --mode 1920x1080 --pos 0x0 --right-of DP-2-1 --rotate normal
+set_wallpaper
