9b2780d12552ddf9b960367acbc297d97e568ecc
Philipp Hansch <dev@phansch.net>
2020-12-10 11:12:17 +0100

    vim: Update to use 'lspconfig'

--- vim/.config/nvim/lua/lsp/init.lua
+++ vim/.config/nvim/lua/lsp/init.lua
@@ -4,10 +4,10 @@ local on_attach = function(client, bufnr)
   completion.on_attach(client, bufnr)
 end
 
-require'nvim_lsp'.solargraph.setup{
+require'lspconfig'.solargraph.setup{
   on_attach = on_attach,
 }
-require'nvim_lsp'.rust_analyzer.setup{
+require'lspconfig'.rust_analyzer.setup{
   on_attach = on_attach,
 }
 
