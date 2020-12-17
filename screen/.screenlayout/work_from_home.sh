94affa528f3ea8ccb2f39a6287ee432be95f72c7
Philipp Hansch <dev@phansch.net>
2020-12-17 16:32:27 +0100

    screenlayout: Fix scripts not working correctly

--- screen/.screenlayout/work_from_home.sh
+++ screen/.screenlayout/work_from_home.sh
@@ -5,5 +5,5 @@
 xrandr --output DP-1-1 --off
 xrandr && xrandr \
   --output DP-1-1 --mode 1920x1080 --pos 0x0 --primary --rotate normal \
-  --output eDP-1 --mode 1920x1080 --pos 0x0 --right-of DP-2-1 --rotate normal
+  --output eDP-1 --mode 1920x1080 --pos 0x0 --right-of DP-1-1 --rotate normal
 set_wallpaper
