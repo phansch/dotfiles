1ed8d4932446d15247d5132891a8851c900fd518
Philipp Hansch <dev@phansch.net>
2024-09-23 20:31:25 +0200

    Fix vim-tmux-navigator

--- vim/.config/nvim/lua/config/autocmds.lua
+++ vim/.config/nvim/lua/config/autocmds.lua
@@ -2,16 +2,16 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local function augroup(name)
-	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
+-- local function augroup(name)
+-- 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+-- end
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = augroup("filetype_markdown"),
-	pattern = { "md", "markdown" },
-	callback = function()
-		vim.opt_local.textwidth = 80
-		vim.opt_local.nofoldenable = true
-		vim.opt_local.wrap = true
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+-- 	group = augroup("filetype_markdown"),
+-- 	pattern = { "md", "markdown" },
+-- 	callback = function()
+-- 		vim.opt_local.textwidth = 80
+-- 		vim.opt_local.nofoldenable = true
+-- 		vim.opt_local.wrap = true
+-- 	end,
+-- })
