f9198005db656eb8cb48477aa1788ab875959849
Philipp Hansch <dev@phansch.net>
2024-11-02 23:03:55 +0100

    vim: Add neotest
    New keybinds at `<space>t` for running ruby tests

--- vim/.config/nvim/lua/config/keymaps.lua
+++ vim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,8 @@ local map = vim.keymap.set
 map("n", "<S-h>", "^", { desc = "Start of line" })
 map("n", "<S-l>", "$", { desc = "End of line" })
 map("v", "<S-l>", "g_", { desc = "End of line" })
+
+vim.keymap.set("n", "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", { desc = "RSpec on file" })
+vim.keymap.set("n", "<leader>tl", ":lua require('neotest').run.run()<CR>", { desc = "RSpec on test" })
+vim.keymap.set("n", "<leader>to", ":lua require('neotest').output.open({enter = true})<CR>", { desc = "RSpec output" })
+vim.keymap.set("n", "<leader>tp", ":lua require('neotest').summary.open()<CR>", { desc = "RSpec summary" })
