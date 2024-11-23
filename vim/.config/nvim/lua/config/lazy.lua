686c78a3d27163f2f3d48401a3c3b11a02c14d6f
Philipp Hansch <dev@phansch.net>
2024-11-23 13:55:51 +0100

    vim: Add codeium plugin

--- vim/.config/nvim/lua/config/lazy.lua
+++ vim/.config/nvim/lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.ai.codeium" },
 		{ import = "plugins" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
