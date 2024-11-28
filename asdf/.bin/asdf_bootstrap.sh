8941d4fc0605234a6671f1c703bcfb1543f785b7
Philipp Hansch <philipp.hansch@easybill.de>
2024-11-28 15:23:08 +0100

    Add asdf to setup script
    Mostly because at that point a specific python version is needed.

---
+++ asdf/.bin/asdf_bootstrap.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+
+. "$HOME/.asdf/asdf.sh"
+
+asdf plugin add python
+asdf plugin add ruby
+
+asdf install python 3.13
+asdf install ruby latest
