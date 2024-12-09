901da5db37f5b199927aca6d383d62b245f5276d
Philipp Hansch <philipp.hansch@easybill.de>
2024-12-09 20:45:53 +0100

    vim: Enable rule, disable relativenumber

--- vim/.config/nvim/lua/config/options.lua
+++ vim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,13 @@ vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "standardrb"
 
 vim.g.lazyvim_picker = "fzf"
+
+local opt = vim.opt
+
+opt.ruler = true
+opt.relativenumber = false
+
+-- Make sure that comments are automatically wrapped at 'textwidth'.
+-- See :help fo-table for details on the options.
+opt.textwidth = 78
+opt.formatoptions = "jcroq"
