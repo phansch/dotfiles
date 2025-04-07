220ff6019fac5c34324398929ff40ec434666151
Philipp Hansch <dev@phansch.net>
2025-04-07 13:29:43 +0200

    Update asdf to >0.16
    Lots of breaking changes.

--- asdf/.bin/asdf_bootstrap.sh
+++ asdf/.bin/asdf_bootstrap.sh
@@ -2,22 +2,25 @@
 
 set -x
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+version="v0.16.7"
+wget "https://github.com/asdf-vm/asdf/releases/download/$version/asdf-$version-linux-amd64.tar.gz" -P /tmp/
+mkdir -p /opt/asdf/
+tar -xvf /tmp/asdf-$version-linux-amd64.tar.gz -C /opt/asdf/
 
-# shellcheck disable=SC1091
-. "$HOME/.asdf/asdf.sh"
+asdf="/opt/asdf/asdf"
+$asdf info
 
-asdf plugin add python
-asdf plugin add ruby
-asdf plugin add nodejs
-asdf plugin add rust
+$asdf plugin add python
+$asdf plugin add ruby
+$asdf plugin add nodejs
+$asdf plugin add rust
 
-asdf install python latest
-asdf install rust latest
-export RUBY_CONFIGURE_OPTS=--enable-yjit
-asdf install ruby latest
-asdf install nodejs latest
+$asdf install python latest
+$asdf install python 3.13.2
+$asdf install rust latest
+$asdf install ruby latest
+$asdf install nodejs latest
 
-asdf global nodejs latest
-asdf global ruby latest
-asdf global python latest
+$asdf set --home nodejs latest
+$asdf set --home ruby latest
+$asdf set --home python latest
