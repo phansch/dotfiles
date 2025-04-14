f8cb9e34ff00dc2ad0a4b404a0f2829128b5dfac
Philipp Hansch <philipp.hansch@easybill.de>
2025-04-14 09:04:40 +0200

    vim: Add nvim-surround
    Been missing this a lot. If I remember correctly, the last time I tried
    it, it didn't work correctly. Now it seems to work.

--- vim/.config/nvim/lua/plugins/base.lua
+++ vim/.config/nvim/lua/plugins/base.lua
@@ -28,6 +28,15 @@ return {
 		end,
 	},
 
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+
 	-- Git
 	"rhysd/git-messenger.vim",
 
