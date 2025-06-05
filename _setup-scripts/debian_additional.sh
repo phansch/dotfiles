7f7f57bce7900d762ad09779c693d9a0489a38ba
Philipp Hansch <dev@phansch.net>
2025-06-05 21:42:15 +0200

    Setup: Install borgbackup CLI

--- _setup-scripts/debian_additional.sh
+++ _setup-scripts/debian_additional.sh
@@ -7,7 +7,7 @@ source _setup-scripts/utils.sh
 # Use git-ppa for most recent release
 run_with_sudo add-apt-repository -y ppa:git-core/ppa
 run_with_sudo apt-get update
-run_with_sudo apt-get install -y -qq calibre flatpak unclutter flameshot jq krita redshift git
+run_with_sudo apt-get install -y -qq calibre flatpak unclutter flameshot jq krita redshift git borgbackup
 
 # Install PikaBackup
 # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
